b497ce2682d8f03e511d43ae9f0ba3e03e18e7cc
vertelbot <vertelbot@vertel.se>
2025-03-20 10:38:07 +0000

    odoobranchpfile odoo-csrd from 16.0. Do not p-file sync

--- csrd_esrs/models/csrd_esrs_category.p.py
+++ csrd_esrs/models/csrd_esrs_category.p.py
@@ -35,7 +35,11 @@ class CSRDESRSCategory(models.Model):
     priority = fields.Selection(selection=[("0","0"),("1","1")])
 
     @api.model
+    # #if VERSION <= "17.0"
+    def _read_group_impact_materiality(self, values, domain, order):
+    # #elif VERSION >= "18.0"
     def _read_group_impact_materiality(self, values, domain):
+    # #endif
         all_values = [value[0] for value in self._fields['impact_materiality'].selection]
 
         # Make sure values contains all possible selection values
@@ -47,7 +51,11 @@ class CSRDESRSCategory(models.Model):
         return values
 
     @api.model
+    # #if VERSION <= "17.0"
+    def _read_group_financial_materiality(self, values, domain, order):
+    # #elif VERSION >= "18.0"
     def _read_group_financial_materiality(self, values, domain):
+    # #endif
         all_values = [value[0] for value in self._fields['financial_materiality'].selection]
 
         missing_values = set(all_values) - set(values)
