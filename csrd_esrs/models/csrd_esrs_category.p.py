d874890c1a9a7193b56d6cc8f98073ee3850743a
vertelbot <vertelbot@vertel.se>
2025-03-20 08:23:10 +0000

    odoobranchpfile odoo-csrd from 18.0. Do not p-file sync

--- csrd_esrs/models/csrd_esrs_category.p.py
+++ csrd_esrs/models/csrd_esrs_category.p.py
@@ -24,12 +24,13 @@ class CSRDESRSCategory(models.Model):
     impact_materiality = fields.Selection([
         ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'),
         ('10', '10'),
-    ], string="Impact Materiality", default=None, group_operator="max")
+    ], string="Impact Materiality", default=None, group_operator="max", group_expand='_read_group_impact_materiality')
 
     financial_materiality = fields.Selection([
         ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'),
         ('10', '10'),
-    ], string="Financial Materiality", default=None, group_operator="max")
+    ], string="Financial Materiality", default=None, group_operator="max",
+        group_expand='_read_group_financial_materiality')
     
     priority = fields.Selection(selection=[("0","0"),("1","1")])
 
@@ -77,7 +78,6 @@ class CSRDESRSCategory(models.Model):
         children = self.search([("parent_id", "child_of", self.id)])
         for child in children:
             if child.id != self.id:
-                _logger.error(f"{child.name=}"*50)
                 child.impact_materiality = child.parent_id.impact_materiality
                 child.financial_materiality = child.parent_id.financial_materiality
 
