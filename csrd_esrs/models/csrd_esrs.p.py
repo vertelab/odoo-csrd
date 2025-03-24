ec3932390253a3ab0d7afffa7e07b2bc3df276c2
vertelbot <vertelbot@vertel.se>
2025-03-24 07:41:13 +0000

    odoobranchpfile odoo-csrd from 18.0. Do not p-file sync

--- csrd_esrs/models/csrd_esrs.p.py
+++ csrd_esrs/models/csrd_esrs.p.py
@@ -75,12 +75,27 @@ class CSRDESRS(models.Model):
             if category_id:
                 rec.category_id = category_id
 
+    ## if VERSION <= "17.0"
     impact_materiality = fields.Selection([
         ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'),
         ('10', '10'),
     ], string="Impact Materiality", default=None, group_operator="max")
+    ## else
+    impact_materiality = fields.Selection([
+        ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'),
+        ('10', '10'),
+    ], string="Impact Materiality", default=None, aggregator="max")
+    ##endif
 
+    ## if VERSION <= "17.0"
     financial_materiality = fields.Selection([
         ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'),
         ('10', '10'),
     ], string="Financial Materiality", default=None, group_operator="max")
+    ## else
+    financial_materiality = fields.Selection([
+        ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'),
+        ('10', '10'),
+    ], string="Financial Materiality", default=None, aggregator="max")
+    ##endif
+
